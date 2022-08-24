44542c984ff5fcf0b9235df62bcc27cb1f0fc4e7
ig94 <ignatiustan94@gmail.com>
2022-08-24 23:24:42 +0800

    search component

---
+++ react-onest-app/src/components/search/Search.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { ApiData } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadAreas = () => {
+    return fetch(
+      "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast",
+      ApiData
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.items[0].forecasts.map((area) => {
+            return {
+              value: `${area.forecast}`,
+              label: `${area.area}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for area"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadAreas}
+    />
+  );
+};
+
+export default Search;
