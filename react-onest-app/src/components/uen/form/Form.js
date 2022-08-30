85f193fef1973e7974b3cd85cd34b5bfa41571cf
ig94 <ignatiustan94@gmail.com>
2022-08-30 20:45:01 +0800

    pattern changes
    to include businesses and local companies registered with ACRA before 2009

--- react-onest-app/src/components/uen/form/Form.js
+++ react-onest-app/src/components/uen/form/Form.js
@@ -21,7 +21,7 @@ const Form = () => {
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <h1 className={styles.validation}>New UEN Validation</h1>
+          <h1 className={styles.validation}>UEN Validation</h1>
         </div>
         <div>
           <label className={styles.uen}>UEN</label>
@@ -31,13 +31,13 @@ const Form = () => {
             {...register("uen", {
               required: true,
               pattern:
-                /^[TSR]\d{2}(LP|LL|FC|PF|RF|MQ|MM|NB|CC|CS|MB|FM|GS|DP|CP|NR|CM|CD|MD|HS|VH|CH|MH|CL|XL|CX|HC|RP|TU|TC|FB|FN|PA|PB|SS|MC|SM|GA|GB)\d{4}[A-Z]$/i,
+                /(^[TSR]\d{2}(LP|LL|FC|PF|RF|MQ|MM|NB|CC|CS|MB|FM|GS|DP|CP|NR|CM|CD|MD|HS|VH|CH|MH|CL|XL|CX|HC|RP|TU|TC|FB|FN|PA|PB|SS|MC|SM|GA|GB)\d{4}[A-Z]$)|(^\d{8}[A-Z]$)|(^(18|19|20)\d{7}[A-Z]$)/i,
             })}
           />
           <p className={styles.error}>
             {errors.uen?.type === "required" && "UEN is required"}
             {errors.uen?.type === "pattern" &&
-              "Entered UEN is in wrong format. UEN should be in TyyPQnnnnX format, where 'Tyy' / 'Syy' / 'yyyy'= year of issuance, T' represents '20', 'S' represents '19' and 'R' represents '18'. E.g. T08 means year 2008, S99 means year 1999 and R00 means year 1800; 'PQ' = Entity-type where 'P'= a alphabetical letter, 'Q' = an alpha-numeric digit, E.g. 'LL'= 'Limited Liability Partnership'; 'n' = a number; 'X' = a check alphabet; For example, the UEN for a limited liability partnership (LLP) formed on 1 January 2009 could be 'T09LL0001B'."}
+              "Entered UEN is in wrong format. UEN should be in nnnnnnnnX (Businesses registered with ACRA before 2009) or yyyynnnnnX (Local companies registered with ACRA before 2009) or TyyPQnnnnX (new UEN) format, where 'Tyy' / 'Syy' / 'yyyy'= year of issuance, T' represents '20', 'S' represents '19' and 'R' represents '18'. E.g. T08 means year 2008, S99 means year 1999 and R00 means year 1800; 'PQ' = Entity-type where 'P'= a alphabetical letter, 'Q' = an alpha-numeric digit, E.g. 'LL'= 'Limited Liability Partnership'; 'n' = a number; 'X' = a check alphabet; For example, the UEN for a limited liability partnership (LLP) formed on 1 January 2009 could be 'T09LL0001B'."}
           </p>
         </div>
         <div>
